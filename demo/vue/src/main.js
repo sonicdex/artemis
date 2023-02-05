4fdba840a49b8a78ae338de7fc8ed63e5d27be0a
MemeCake <support@memecake.io>
2023-02-05 13:56:30 +0530

    core lib funcitons

--- demo/vue/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import artemis from "./Lib/artemis";
-
-const app = createApp(App);
-app.config.globalProperties.artemis = artemis;
-
-app.mount("#app");
--- demo/vue/vue.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  lintOnSave: false,
-  transpileDependencies: [
-    '@dfinity/agent', '@dfinity/candid', '@dfinity/principal', '@dfinity/identity',
-  ]
-})

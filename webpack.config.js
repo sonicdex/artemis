98ead6b6a5fa44e385dfdb1ad64efefe74db4fe7
MemeCake <support@memecake.io>
2023-02-07 13:02:13 +0530

    bug fixes webpack config

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+module.exports={
+    mode:"development",
+    entry: path.resolve(__dirname,'src/index.js'),
+    output:{
+        path: path.resolve(__dirname,'dist'),
+        filename:"bundle.js"
+    }
+}

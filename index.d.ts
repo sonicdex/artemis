c5f8bb3d4b81efe6671c236a25ce85c118ec0f19
memecakedev <support@memecake.io>
2023-03-23 16:18:05 +0530

    d update

--- index.d.ts
+++ index.d.ts
@@ -5,9 +5,11 @@ export default class Artemis {
     provider:any;
     balance:number;
     wallets:[];
+    canisterActors:{};
     connect(wallet: string, connectObj?:any): string;
     disconnect():boolean;
     isLoaded():boolean;
     getWalletBalance():number;
     requestICPTransfer(transferRequest:any):any;
+    getCanisterActor(canisterId:string, idl:any,isAnon:boolean):any;
 }

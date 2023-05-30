0e4ee89ca0796a3dfdf78ff3eac6f3514ae36c66
jomongeorge <jomongeorge23@gmail.com>
2023-05-30 11:13:17 +0530

    add wallet astroxme init

--- src/wallets/wallet-list.js
+++ src/wallets/wallet-list.js
@@ -6,8 +6,9 @@ import { dfinity } from './dfinity';
 const wallets = { plug, stoic, bitfinity, dfinity };
 
 export const walletList = [
+    { id: 'dfinity', name: "Internet Identity", icon: 'https://raw.githubusercontent.com/sonicdex/artemis/main/assets/dfinity.svg', adapter: wallets.dfinity },
+    { id: 'astroxme', name: 'AstroX ME', icon: 'https://raw.githubusercontent.com/sonicdex/artemis/main/assets/astroxme.svg', adapter: false },
+    { id: 'bitfinity', name: 'Bitfinity Wallet', icon: 'https://raw.githubusercontent.com/sonicdex/artemis/main/assets/bitfinity.svg', adapter: wallets.bitfinity },
     { id: 'plug', name: 'Plug Wallet', icon: 'https://raw.githubusercontent.com/sonicdex/artemis/main/assets/plug.jpg', adapter: wallets.plug },
     { id: 'stoic', name: 'Stoic Wallet', icon: 'https://raw.githubusercontent.com/sonicdex/artemis/main/assets/stoic.png', adapter: wallets.stoic },
-    { id: 'bitfinity', name: 'Bitfinity Wallet', icon: 'https://raw.githubusercontent.com/sonicdex/artemis/main/assets/bitfinity.svg', adapter: wallets.bitfinity },
-    { id: 'dfinity', name: "Internet Identity", icon: 'https://raw.githubusercontent.com/sonicdex/artemis/main/assets/dfinity.svg', adapter: wallets.dfinity },
 ];

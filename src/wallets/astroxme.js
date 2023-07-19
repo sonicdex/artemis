09a662dd6288fd278a39343632040ef624784bf0
jomongeorge <jomongeorge23@gmail.com>
2023-07-19 09:56:30 +0530

    bug fix plug

--- src/wallets/astroxme.js
+++ src/wallets/astroxme.js
@@ -1,14 +1,17 @@
-import { HttpAgent } from '@dfinity/agent';
+import { Actor, HttpAgent } from '@dfinity/agent';
+
 import { getAccountIdentifier } from '../libs/identifier-utils';
 import { IC } from "@astrox/sdk-web";
 import { AstroXWebViewHandler } from '@astrox/sdk-webview';
 import { Principal } from '@dfinity/principal'
+
+
 window.icx = new AstroXWebViewHandler();
 
 const astroxConfig = {
     providerUrl: 'https://63k2f-nyaaa-aaaah-aakla-cai.raw.ic0.app',
     delegationModes: ["global"],
-    ledgerHost: "https://boundary.ic0.app/",
+    ledgerHost: "https://icp0.io/",
 }
 
 const astroxInit = async (host = '', whitelist = []) => {
@@ -31,26 +34,40 @@ if (!window.ic?.astrox) astroxInit()
 export const astrox = {
     readyState: "Loadable", url: "https://63k2f-nyaaa-aaaah-aakla-cai.raw.ic0.app",
     connectWallet: async function (connectObj = { whitelist: [], host: '' }) {
+
+        console.log(connectObj);
+
         var self = this;
         return new Promise(async (resolve, reject) => {
             //check app is  in ME App.
             try { await window.icx.init(); } catch (e) { window.icx = false };
             const isICXReady = window.icx._isReady;
             if (isICXReady) {
+                await astroxInit(connectObj.host  , connectObj.whitelist);
+                
                 const isconneted = await window.icx.isConnected();
                 if (!isconneted) {
-                    await window.icx.connect({ ...window.ic.astrox.connectOptions, delegationTargets: connectObj.whitelist, ledgerHost: connectObj.host })
+                    await window.icx.connect({ ...astroxConfig, delegationTargets: connectObj.whitelist, ledgerHost: connectObj.host })
                 }
+
                 var sid = window.icx.wallet.accountId;
                 self.agent = new HttpAgent({ identity: window.icx.identity, host: connectObj.host });
 
+                // self.createActor = async function (connObj = { canisterId: '', interfaceFactory: false }) {
+                //     if (!connObj.canisterId || !connObj.interfaceFactory) return false;
+                //     return await Actor.createActor(connObj.interfaceFactory, { agent: this.agent, canisterId: connObj.canisterId });
+                // };
+
+
                 self.createActor = async function (connObj = { canisterId: '', interfaceFactory: false }) {
                     if (!connObj.canisterId || !connObj.interfaceFactory) return false;
-                    return await window.icx.createActor(connObj.canisterId, connObj.interfaceFactory);
+                    const actor = await window.icx.createActor(connObj.canisterId, connObj.interfaceFactory);
+                    return actor;
                 };
                 self.getPrincipal = async function (t1) { return Principal.fromText(window.icx.wallet.principal) };
                 self.disConnectWallet = async function () { await window.icx.disconnect(); }
                 resolve({ accountId: sid, principalId: window.icx.wallet.principal });
+
             } else {
                 if (!window.ic?.astrox) {
                     await astroxInit()
@@ -61,11 +78,14 @@ export const astrox = {
                     await window.ic.astrox.connect({ ...window.ic.astrox.connectOptions, delegationTargets: connectObj.whitelist, ledgerHost: connectObj.host })
                 }
                 var sid = await getAccountIdentifier(window.ic.astrox.principal.toString());
+
                 self.agent = new HttpAgent({ identity: window.ic.astrox.identity, host: connectObj.host });
+
                 self.createActor = async function (connObj = { canisterId: '', interfaceFactory: false }) {
                     if (!connObj.canisterId || !connObj.interfaceFactory) return false;
                     return await window.ic.astrox.createActor(connObj.interfaceFactory, connObj.canisterId);
                 };
+
                 self.getPrincipal = async function (t1) { return window.ic.astrox.principal };
                 self.disConnectWallet = async function () { await window.ic.astrox.disconnect() }
                 resolve({ accountId: sid, principalId: window.ic.astrox.principal.toString() });
